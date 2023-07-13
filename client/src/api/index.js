337dc98da0cbacfca9c6ce5f58b6713851614334
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-07-13 23:06:39 +0530

    api changed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://memories-backend-std2.onrender.com",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
